c64f007d846631812bfacdd952ef5839a5f752e6
blab1t <lando.sachs@gmail.com>
2026-03-12 14:07:12 +0100

    removed  stars from hisotical

--- src/minecraft/commands/daily.ts
+++ src/minecraft/commands/daily.ts
@@ -16,7 +16,8 @@ export const dailyCommand: MinecraftCommand = {
 
         const d = data.daily;
         const stars = d.current_stars.toFixed(0);
-        const resp = `/msg ${sender} [${stars}✫] ${d.display_name} [DAILY]: +${d.stars_gained.toFixed(2)}✫ * Finals: ${d.final_kills} * FD: ${d.final_deaths} * FKDR: ${d.fkdr.toFixed(2)} * Wins: ${d.wins} * Losses: ${d.losses} * WLR: ${d.wlr.toFixed(2)} * KDR: ${d.kdr.toFixed(2)} * BBLR: ${d.bblr.toFixed(2)}`;
+        // const resp = `/msg ${sender} [${stars}✫] ${d.display_name} [DAILY]: +${d.stars_gained.toFixed(2)}✫ * Finals: ${d.final_kills} * FD: ${d.final_deaths} * FKDR: ${d.fkdr.toFixed(2)} * Wins: ${d.wins} * Losses: ${d.losses} * WLR: ${d.wlr.toFixed(2)} * KDR: ${d.kdr.toFixed(2)} * BBLR: ${d.bblr.toFixed(2)}`;
+        const resp = `/msg ${sender} [${stars}✫] ${d.display_name} [DAILY]: Finals: ${d.final_kills} * FD: ${d.final_deaths} * FKDR: ${d.fkdr.toFixed(2)} * Wins: ${d.wins} * Losses: ${d.losses} * WLR: ${d.wlr.toFixed(2)} * KDR: ${d.kdr.toFixed(2)} * BBLR: ${d.bblr.toFixed(2)}`;
         mcClient.send(resp);
     }
 };
--- src/minecraft/commands/monthly.ts
+++ src/minecraft/commands/monthly.ts
@@ -16,7 +16,8 @@ export const monthlyCommand: MinecraftCommand = {
 
         const d = data.monthly;
         const stars = d.current_stars.toFixed(0);
-        const resp = `/msg ${sender} [${stars}✫] ${d.display_name} [MONTHLY]: +${d.stars_gained.toFixed(2)}✫ * Finals: ${d.final_kills} * FD: ${d.final_deaths} * FKDR: ${d.fkdr.toFixed(2)} * Wins: ${d.wins} * Losses: ${d.losses} * WLR: ${d.wlr.toFixed(2)} * KDR: ${d.kdr.toFixed(2)} * BBLR: ${d.bblr.toFixed(2)}`;
+        // const resp = `/msg ${sender} [${stars}✫] ${d.display_name} [MONTHLY]: +${d.stars_gained.toFixed(2)}✫ * Finals: ${d.final_kills} * FD: ${d.final_deaths} * FKDR: ${d.fkdr.toFixed(2)} * Wins: ${d.wins} * Losses: ${d.losses} * WLR: ${d.wlr.toFixed(2)} * KDR: ${d.kdr.toFixed(2)} * BBLR: ${d.bblr.toFixed(2)}`;
+        const resp = `/msg ${sender} [${stars}✫] ${d.display_name} [MONTHLY]: Finals: ${d.final_kills} * FD: ${d.final_deaths} * FKDR: ${d.fkdr.toFixed(2)} * Wins: ${d.wins} * Losses: ${d.losses} * WLR: ${d.wlr.toFixed(2)} * KDR: ${d.kdr.toFixed(2)} * BBLR: ${d.bblr.toFixed(2)}`;
         mcClient.send(resp);
     }
 };
